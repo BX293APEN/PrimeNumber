dbfa878f90728442564e85afaeb782b4e6fb1e8d
PEN <uemasaki20011222@gmail.com>
2025-03-12 03:41:21 +0900

    並列処理

--- prime_number.py
+++ prime_number.py
@@ -1,41 +1,66 @@
 import subprocess, time
 from MySQLite import MySQLite
+from multiprocessing import Process, Value
 
+def disp(endFlag):
+    dbFile = "prime_number.db"
+    while endFlag.value == 0:
+        with MySQLite(dbFile) as database:
+            pData = database.send_sql(
+                f"""
+                    SELECT value FROM 素数 WHERE value = (SELECT MAX(value) FROM 素数);
+                """
+            )
+        subprocess.run("cls", shell=True)
+        print(f"{pData[0][0]}")
+        time.sleep(0.01)
 
 
 if __name__ == "__main__":
     target = 2 ** 256
-    with MySQLite("prime_number.db") as database:
+    dbFile = "prime_number.db"
+    endFlag = Value('i', 0)
+    proc = Process(target=disp, args=(endFlag, ))
+    proc.start()
+
+    with MySQLite(dbFile) as database:
         pData = database.send_sql(
             f"""
                 SELECT value FROM 素数 ORDER BY value ASC;
             """
         )
-
+    
     n = int(pData[-1][0])
 
     try:
         while n < target:
-            subprocess.run("cls", shell=True)
             flag = 0
-            print(f"{len(str(n))}桁")
             for p in pData:
-                if p[0] ** 2 > n:
-                    break
-                elif (n % p[0]) == 0:
+                if (n % p[0]) == 0:
                     flag = 1
+                    break
+                elif p[0] ** 2 >= n:
                     break
                 
             if flag == 0:
                 try:
-                    with MySQLite("prime_number.db") as database:
+                    with MySQLite(dbFile) as database:
                         database.send_sql(
                             f"""INSERT INTO 素数 (value) VALUES({int(n)})"""
                         )
+                        pData = database.send_sql(
+                            f"""
+                                SELECT value FROM 素数 ORDER BY value ASC;
+                            """
+                        )
                 except Exception as e:
                     print(e)
+                    
             n += 2
             time.sleep(0.01)
             
     except Exception as e:
-        input(e)
+        print(e)
+        with endFlag.get_lock():
+            endFlag.value = 1
+        proc.join()
